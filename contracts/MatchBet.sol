97dac220961feab64e7648b4258499a3663933de
Anton Cheng <anton@coolbitx.com>
2019-09-17 09:39:02 +0800

    Remove API key, put in adopter.js

--- contracts/MatchBet.sol
+++ contracts/MatchBet.sol
@@ -7,7 +7,6 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract GameBet is ChainlinkClient, Ownable {
     uint256 constant private ORACLE_PAYMENT = 1 * LINK;
     string public apiEndpoint = "https://apiv2.apifootball.com/";
-    string private apiKey;
     string public matchId;
     string public homeTeam;
     string public awayTeam;
@@ -22,19 +21,17 @@ contract GameBet is ChainlinkClient, Ownable {
         bool scheduled
     );
 
-    constructor(string _matchId, string _homeTeam, string _awayTeam, string _apiKey){
+    constructor(string _matchId, string _homeTeam, string _awayTeam){
         matchId = _matchId;
         homeTeam = _homeTeam;
         awayTeam = _awayTeam;
-        apiKey = _apiKey;
         setPublicChainlinkToken();
     }
 
-    function initCheckMatchScheduled(address _oracle, string _jobId, string _apiKey) public {
+    function initCheckMatchScheduled(address _oracle, string _jobId) public {
         require(!matchScheduled, "Match already scheduled.");
         Chainlink.Request memory req = buildChainlinkRequest(stringToBytes32(_jobId), this, this.callbackCheckMatchScheduled.selector);
         req.add("match_id", matchId); // required by getMatch
-        req.add("api_key", apiKey);
         req.add("copyPath", "1.match_status");
         sendChainlinkRequestTo(_oracle, req, ORACLE_PAYMENT);
     }
