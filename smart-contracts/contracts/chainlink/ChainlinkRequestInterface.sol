4389d8e2ebb4c30e7c9350e838f988c153521373
Anton Cheng <anton@coolbitx.com>
2019-09-22 18:13:19 +0800

    Refactor folder structure

---
+++ smart-contracts/contracts/chainlink/ChainlinkRequestInterface.sol
@@ -0,0 +1,24 @@
+
+// File: contracts/interfaces/ChainlinkRequestInterface.sol
+
+pragma solidity 0.4.24;
+
+interface ChainlinkRequestInterface {
+  function oracleRequest(
+    address sender,
+    uint256 payment,
+    bytes32 id,
+    address callbackAddress,
+    bytes4 callbackFunctionId,
+    uint256 nonce,
+    uint256 version,
+    bytes data
+  ) external;
+
+  function cancelOracleRequest(
+    bytes32 requestId,
+    uint256 payment,
+    bytes4 callbackFunctionId,
+    uint256 expiration
+  ) external;
+}
