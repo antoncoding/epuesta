a7eaf6e4774ff351db787bd9c57f3e4ac5399317
Anton Cheng <anton@coolbitx.com>
2019-09-28 17:30:08 +0800

    Update adopter to standard api server

--- adopters/footballAdopter.js
+++ adopters/footballAdopter.js
@@ -1,33 +1,17 @@
-let request = require('request');
+let fetch = require('node-fetch');
 
 /**
  * Get specific match detail from https://apifootball.com/
  * @param {{body:{data:{match_id:string}, id:string}}} req
  **/
-exports.matchAdapter = (req, res) => {
+exports.matchAdapter = async (req) => {
   const footballApiKey = "15fe3e60cc53879ea6ab6a0964838e35ae7565cc2a16e4dbce6289bd4dbd85d9"
   const match_id = req.body.data.match_id || "";
-  const url = `https://apiv2.apifootball.com/action=get_events&match_id=${match_id}&APIkey=$${footballApiKey}`;
- 
-  let options = {
-      url: url,
-      json: true
+  const url = `https://apiv2.apifootball.com/?action=get_events&match_id=${match_id}&APIkey=${footballApiKey}`;
+  let res = await fetch(url)
+  let body = await res.json()
+  return {
+    jobRunID: req.body.id,
+    data: body[0],
   };
-
-  request(options, (error, response, body) => {
-    if (error || response.statusCode >= 400) {
-        let errorData = {
-            jobRunID: req.body.id,
-            status: "errored",
-            error: body
-        };
-        res.status(response.statusCode).send(errorData);
-    } else {
-      let returnData = {
-        jobRunID: req.body.id,
-        data: body[0],
-      };
-      res.status(response.statusCode).send(returnData);
-    }
-  });
 };
